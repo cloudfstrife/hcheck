5acfb3f49b73f79de549d475b93e945060ce7796
Cloud Strife <cloudfstrife@sina.cn>
2019-09-30 14:28:15 +0800

    fix: modify check result and Check function ,make each checker parallel

--- checker.go
+++ checker.go
@@ -2,10 +2,18 @@ package hcheck
 
 import "sync"
 
-//Checker wrapped inspect method
+//Checker wrap inspect method
 type Checker interface {
 	//Check function, do something inspect
-	Check() (string, error)
+	Check() Status
+}
+
+//Status wrap check result
+type Status struct {
+	//Pass flag
+	Pass bool
+	//Msg check result message
+	Msg string
 }
 
 var (
@@ -14,18 +22,22 @@ var (
 )
 
 //Check do all check and return check result
-func Check() map[string]string {
-	result := make(map[string]string, len(checklist))
+func Check() map[string]Status {
+	result := make(map[string]Status, len(checklist))
+
+	cmap := make(map[string]chan Status, len(checklist))
 
 	for k, v := range checklist {
-		cr, err := v.Check()
-		if err != nil {
-			result[k] = cr + err.Error()
-		} else {
-			result[k] = cr
-		}
+		sc := make(chan Status, 1)
+		go func(c Checker, cs chan Status) {
+			cs <- c.Check()
+		}(v, sc)
+		cmap[k] = sc
 	}
-
+	for k, v := range cmap {
+		result[k] = <-v
+		close(v)
+	}
 	return result
 }
 
@@ -43,8 +55,8 @@ func Unregister(name string) {
 type GenChecker struct{}
 
 //Check GenChecker return string "OK" and nil error
-func (gen *GenChecker) Check() (string, error) {
-	return "OK", nil
+func (gen *GenChecker) Check() Status {
+	return Status{Pass: true, Msg: "OK"}
 }
 
 func init() {
